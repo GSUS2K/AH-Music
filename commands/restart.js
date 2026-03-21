47ca9a18024afbc0955bfb760eab4a5d61246505
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-03-22 01:45:50 +0530

    fix: restrict /restart to owner only

--- commands/restart.js
+++ commands/restart.js
@@ -1,12 +1,17 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { exec } = require('child_process');
+
+const OWNER_ID = '682288992456409096';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('restart')
-        .setDescription('Restart the bot (Admin only)')
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+        .setDescription('Restart the bot (Owner only)'),
     async execute(interaction) {
+        if (interaction.user.id !== OWNER_ID) {
+            return interaction.reply({ content: 'You are not authorized to use this command.', ephemeral: true });
+        }
+
         await interaction.reply({ content: 'Restarting bot...', ephemeral: true });
 
         exec('pm2 restart AH-Music', (err) => {
